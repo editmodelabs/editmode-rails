93a58b3e9139d0e86e84b663903a328931d86484
Tony Ennis <ennis.tony@gmail.com>
2015-08-17 17:36:31 +0100

    Remove engine

--- lib/chunks.rb
+++ lib/chunks.rb
@@ -4,7 +4,6 @@ require "chunks/version"
 require 'chunks/script_tag_helper'
 require 'chunks/auto_include_filter'
 require 'chunks/railtie' if defined? Rails
-require "chunks/engine"
 
 module Chunks
   # Your code goes here...
