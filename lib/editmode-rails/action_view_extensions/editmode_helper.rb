5808dced56992e44f5e8f349016e6e507da9cf22
tonyennis145 <ennis.tony@gmail.com>
2020-03-06 15:22:06 +0800

    Updates
    - Enable collections
    - Rescue missed chunks

--- lib/editmode-rails/action_view_extensions/editmode_helper.rb
+++ lib/editmode-rails/action_view_extensions/editmode_helper.rb
@@ -18,12 +18,15 @@ module EditModeRails
       end
      
       def chunk_collection(collection_identifier,has_tags=[])
-        
-        url = "#{versioned_api_url}/chunks?collection_identifier=#{collection_identifier}"
-        response = HTTParty.get(url)
-        chunks = response["chunks"]
-        return chunks
-
+        begin 
+          url = "#{versioned_api_url}/chunks?collection_identifier=#{collection_identifier}"
+          response = HTTParty.get(url)
+          chunks = response["chunks"]
+          return chunks
+        rescue => error
+          puts error 
+          []
+        end
       end
 
       def chunk_property(chunk_info,custom_field_identifier=nil,options={})
@@ -42,47 +45,51 @@ module EditModeRails
 
       def chunk_display(label,identifier,options={},custom_field_info={})
         
-        if custom_field_info.present?
-          chunk_content = custom_field_info["value"]
-        else
-          chunk_content = Rails.cache.fetch("bit_#{identifier}") do  
-            url = "#{versioned_api_url}/bits/#{identifier}"
-            response = HTTParty.get(url)
-            chunk_content = response['content']
-          end
-        end
-
-        display_type = options[:display_type] || "span"
-        css_class = options[:css_class]
-        content_type = "plain"
-
-        # Simple check to see if returned chunk contains html. Regex will need to be improved
-        if /<[a-z][\s\S]*>/i.match(chunk_content)
-          content_type = "rich"
-          chunk_content = sanitize chunk_content.html_safe
-        elsif chunk_content.include? "\n"
-          content_type = "rich"
-          renderer = Redcarpet::Render::HTML.new(no_links: true, hard_wrap: true)
-          markdown = Redcarpet::Markdown.new(renderer, extensions = {})
-          chunk_content = markdown.render(chunk_content).html_safe
-        end
-
-        additional_data_properties = custom_field_info.present? ? { :custom_field_identifier => custom_field_info["custom_field_identifier"] } : {}
-
-        case display_type
-        when "span"
-          if content_type == "rich"
-            content_tag(:span, :class => css_class, :data => {:chunk => identifier, :chunk_editable => false}.merge(additional_data_properties) ) do
-              chunk_content
-            end
+        begin 
+          if custom_field_info.present?
+            chunk_content = custom_field_info["value"]
           else
-            content_tag(:span, :class => css_class, :data => {:chunk => identifier, :chunk_editable => true}.merge(additional_data_properties)) do
-              chunk_content
+            chunk_content = Rails.cache.fetch("bit_#{identifier}") do  
+              url = "#{versioned_api_url}/bits/#{identifier}"
+              response = HTTParty.get(url)
+              chunk_content = response['content']
             end
           end
-        when "raw"
-          chunk_content
-        end
+
+          display_type = options[:display_type] || "span"
+          css_class = options[:css_class]
+          content_type = "plain"
+
+          # Simple check to see if returned chunk contains html. Regex will need to be improved
+          if /<[a-z][\s\S]*>/i.match(chunk_content)
+            content_type = "rich"
+            chunk_content = sanitize chunk_content.html_safe
+          elsif chunk_content.include? "\n"
+            content_type = "rich"
+            renderer = Redcarpet::Render::HTML.new(no_links: true, hard_wrap: true)
+            markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+            chunk_content = markdown.render(chunk_content).html_safe
+          end
+
+          additional_data_properties = custom_field_info.present? ? { :custom_field_identifier => custom_field_info["custom_field_identifier"] } : {}
+
+          case display_type
+          when "span"
+            if content_type == "rich"
+              content_tag(:span, :class => css_class, :data => {:chunk => identifier, :chunk_editable => false}.merge(additional_data_properties) ) do
+                chunk_content
+              end
+            else
+              content_tag(:span, :class => css_class, :data => {:chunk => identifier, :chunk_editable => true}.merge(additional_data_properties)) do
+                chunk_content
+              end
+            end
+          when "raw"
+            chunk_content
+          end
+       rescue => error
+        puts error
+       end
 
       end
 
--- lib/editmode-rails/version.rb
+++ lib/editmode-rails/version.rb
@@ -1,3 +1,3 @@
 module EditModeRails
-  VERSION = "0.0.9.37"
+  VERSION = "0.0.9.39"
 end
