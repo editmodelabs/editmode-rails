d8af36bd561860f940be5969fbfc70f2cefece20
Karamat Ullah <karamatullah46@gmail.com>
2020-08-28 15:49:42 +0500

    second arguments of  method can be a field name or options hash

--- lib/editmode/engine.rb
+++ lib/editmode/engine.rb
@@ -3,17 +3,32 @@ require 'editmode/chunk_value'
 module Editmode
   class Engine < Rails::Engine
     ActionController::Base.class_eval do
-      def e(identifier, options = {})
+      def e(identifier, *args)
+        field, options = parse_arguments(args)
         begin
-          chunk = Editmode::ChunkValue.new(identifier, **options)
+          chunk = Editmode::ChunkValue.new(identifier, options)
           if chunk.chunk_type == 'collection_item'
-            chunk.field(options[:field])
+            chunk.field(field)
           else
             chunk.content
           end 
         rescue => er
           raise er
         end
+      end
+
+      private
+
+      def parse_arguments(args)
+        field = nil
+        options = {}
+        if args[0].class.name == 'String'
+          field = args[0]
+          options =  args[1] || {}
+        elsif args[0].class.name == 'Hash'
+          options =  args[0] || {}
+        end
+        return field, options
       end
     end
   end
