d7f01c28bdf9144c743e121fe08507b88e69ec2d
Karamat Ullah <karamatullah46@gmail.com>
2020-08-28 12:38:29 +0500

    added  mothod short syntax for controllers

--- lib/editmode/engine.rb
+++ lib/editmode/engine.rb
@@ -1,8 +1,20 @@
+require 'editmode/chunk_value'
+
 module Editmode
-
   class Engine < Rails::Engine
-
-  
+    ActionController::Base.class_eval do
+      def e(identifier, options = {})
+        begin
+          chunk = Editmode::ChunkValue.new(identifier, **options)
+          if chunk.chunk_type == 'collection_item'
+            chunk.field(options[:field])
+          else
+            chunk.content
+          end 
+        rescue => er
+          raise er
+        end
+      end
+    end
   end
-
 end
